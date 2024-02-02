45fdccc876f30f3029b1e0d61250143b4c6d50e2
Andre Copetti <andrecopetti@gmail.com>
2024-02-02 11:32:53 -0300

    adding validation rules to registration form

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,48 @@ export default {
 		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
 	],
   theme: {
-    extend: {},
+	extend: {},
   },
   plugins: [
 		forms,
 		typo,
 		require("daisyui")
 	],
+	daisyui: {
+		themes: [
+			"light",
+			"dark",
+			"cupcake",
+			"bumblebee",
+			"emerald",
+			"synthwave",
+			"synthwave",
+			"retro",
+			"cyberpunk",
+			"valentine",
+			"halloween",
+			"garden",
+			"forest",
+			"aqua",
+			"lofi",
+			"pastel",
+			"fantasy",
+			"wireframe",
+			"black",
+			"luxury",
+			"dracula",
+			"cmyk",
+			"autumn",
+			"business",
+			"acid",
+			"lemonade",
+			"night",
+			"coffee",
+			"winter",
+			"dim",
+			"nord",
+			"sunset",
+		],
+	}
 }
 
---
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Livewire\Auth\Register;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Livewire\Livewire;
+
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
+
+it('should render the component', function () {
+    Livewire::test(Register::class)
+        ->assertOk();
+});
+
+it('should be able to register a new user in the system', function () {
+    Livewire::test(Register::class)
+        ->set('name', 'Joe doe')
+        ->set('email', 'joe@doe.com')
+        ->set('email_confirmation', 'joe@doe.com')
+        ->set('password', 'password')
+        ->call('submit')
+        ->assertHasNoErrors()
+        ->assertRedirect(RouteServiceProvider::HOME);
+
+    $this->assertDatabaseHas('users', [
+        'name'  => 'Joe Doe',
+        'email' => 'joe@doe.com',
+    ]);
+
+    $this->assertDatabaseCount('users', 1);
+
+    $this->expect(auth()->check())
+        ->and(auth()->user())
+        ->id->toBe(User::first()->id);
+});
+
+test('validation rules', function ($f) {
+    Livewire::test(Register::class)
+        ->set($f->field, $f->value)
+        ->call('submit')
+        ->assertHasErrors([$f->field => $f->rule]);
+})->with([
+    'name::required'     => (object)['field' => 'name', 'value' => '', 'rule' => 'required'],
+    'name::max:255'      => (object)['field' => 'name', 'value' => str_repeat('*', 256), 'rule' => 'max'],
+    'email::required'    => (object)['field' => 'email', 'value' => '', 'rule' => 'required'],
+    'email::email'       => (object)['field' => 'email', 'value' => 'not-an-email', 'rule' => 'email'],
+    'email::max:255'     => (object)['field' => 'email', 'value' => str_repeat('*' . '@doe.com', 256), 'rule' => 'max'],
+    'email::confirmed'   => (object)['field' => 'email', 'value' => 'joe@doe.com', 'rule' => 'confirmed'],
+    'password::required' => (object)['field' => 'password', 'value' => '', 'rule' => 'required'],
+]);

aec724602cea91870e2c83519c737c4d667dc051
Andre Copetti <andrecopetti@gmail.com>
2024-02-08 18:04:49 -0300

    ensuring login to be secured by rate limiting

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,15 +2,19 @@
 
 namespace App\Livewire\Auth;
 
-use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\View\{View};
+use Illuminate\Support\Facades\{Auth, RateLimiter};
+use Illuminate\Support\Str;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Login extends Component
 {
-    public ?string $email;
-    public ?string $password;
+    public ?string $email = null;
 
+    public ?string $password = null;
+
+    #[Layout('components.layouts.guest')]
     public function render(): View
     {
         return view('livewire.auth.login');
@@ -18,13 +22,36 @@ class Login extends Component
 
     public function tryToLogin(): void
     {
+        if($this->ensureIsNotRateLimiting()) {
+            return;
+        }
 
-        if(!Auth::attempt(['email' => $this->email, 'password' => $this->password])){
+        if (!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            RateLimiter::hit($this->throttleKey());
 
             $this->addError('invalidCredentials', trans('auth.failed'));
+
             return;
         }
 
         $this->redirect(route('dashboard'));
+    }
+
+    private function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->email) . '|' . request()->ip());
+    }
+
+    private function ensureIsNotRateLimiting(): bool
+    {
+        if (RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            $this->addError('rateLimiter', trans('auth.throttle', [
+                'seconds' => RateLimiter::availableIn($this->throttleKey()),
+            ]));
+
+            return true;
+        }
+
+        return false;
     }
 }

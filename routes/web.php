815d0cbf7412d3679ae9afd0ca14afc362238f91
Andre Copetti <andrecopetti@gmail.com>
2024-02-09 10:58:47 -0300

    frontend of our logout button

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('auth.register');
-Route::get('/logout', fn() => auth()->logout());
+Route::get('/logout', fn () => auth()->logout());
 
 Route::middleware('auth')->group(function (){
     Route::get('/', Welcome::class)->name('dashboard');

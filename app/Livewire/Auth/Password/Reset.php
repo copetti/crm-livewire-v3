5e17f8a8aa837041a8b0aa63d3fdb2672769e02c
André Copetti <andre.copetti_t@eloca.com.br>
2024-07-02 14:36:39 -0300

    making sure that password is being updated

--- app/Livewire/Auth/Password/Reset.php
+++ app/Livewire/Auth/Password/Reset.php
@@ -2,18 +2,27 @@
 
 namespace App\Livewire\Auth\Password;
 
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Reset extends Component
 {
     public ?string $token = null;
+    public ?string $email = null;
+    public ?string $email_confirmation = null;
+    public ?string $password = null;
+    public ?string $password_confirmation = null;
 
-    public function mount(): void
+    public function mount(?string $token, ?string $email): void
     {
-        $this->token = request('token');
+        $this->token = request('token', $token);
+        $this->email = request('email', $email);
 
         if($this->tokenNotValid()){
             session()->flash('status', 'Token Invalid');
@@ -24,6 +33,22 @@ class Reset extends Component
     public function render(): View
     {
         return view('livewire.auth.password.reset');
+    }
+
+    public function updatePassword(): void
+    {
+        $status = Password::reset(
+            $this->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, $password){
+                $user->password = $password;
+                $user->remember_token = Str::random(60);
+                $user->save();
+            }
+        );
+
+        session()->flash('status', __($status));
+
+        $this->redirect(route('dashboard'));
     }
 
     private function tokenNotValid(): bool
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com'
+            'name' => 'André',
+            'email' => 'andrecopetti@gmail.com'
         ]);
     }
 }

1b8927e4d223c245e1a64f4060db05e72c1577bf
André Copetti <andre.copetti_t@eloca.com.br>
2024-07-02 14:57:10 -0300

    checking form rules

--- app/Livewire/Auth/Password/Reset.php
+++ app/Livewire/Auth/Password/Reset.php
@@ -2,34 +2,42 @@
 
 namespace App\Livewire\Auth\Password;
 
+use App\Models\User;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\{DB, Hash, Password};
 use Illuminate\Support\Str;
-use Livewire\Attributes\Layout;
+use Livewire\Attributes\{Computed, Layout, Rule};
 use Livewire\Component;
 
 class Reset extends Component
 {
     public ?string $token = null;
+
+    #[Rule(['required', 'email', 'confirmed'])]
     public ?string $email = null;
+
     public ?string $email_confirmation = null;
+
+    #[Rule(['required', 'confirmed'])]
     public ?string $password = null;
+
     public ?string $password_confirmation = null;
 
-    public function mount(?string $token, ?string $email): void
+    public function mount(?string $token = null, ?string $email = null): void
     {
         $this->token = request('token', $token);
         $this->email = request('email', $email);
 
-        if($this->tokenNotValid()){
+        if ($this->tokenNotValid()) {
+
             session()->flash('status', 'Token Invalid');
+
             $this->redirectRoute('login');
         }
     }
 
+    #[Layout('components.layouts.guest')]
     public function render(): View
     {
         return view('livewire.auth.password.reset');
@@ -37,18 +45,32 @@ class Reset extends Component
 
     public function updatePassword(): void
     {
+        $this->validate();
+
         $status = Password::reset(
             $this->only('email', 'password', 'password_confirmation', 'token'),
-            function (User $user, $password){
-                $user->password = $password;
+            function (User $user, $password) {
+                $user->password       = $password;
                 $user->remember_token = Str::random(60);
                 $user->save();
+
+                event(new PasswordReset($user));
             }
         );
 
         session()->flash('status', __($status));
 
-        $this->redirect(route('dashboard'));
+        if ($status !== Password::PASSWORD_RESET) {
+            return;
+        }
+
+        $this->redirect(route('login'));
+    }
+
+    #[Computed]
+    public function obfuscatedEmail(): string
+    {
+        return obfuscate_email($this->email);
     }
 
     private function tokenNotValid(): bool
